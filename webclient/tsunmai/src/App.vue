4aead2e92b6d7c39434eca133c911aa6f6ddd5fe
seriousSam <stupidanimal@outlook.com>
2020-04-05 17:34:26 +0800

    合并其他

--- webclient/tsunmai/src/App.vue
+++ webclient/tsunmai/src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <el-container>
-      <!-- <el-container> -->
-      <el-aside>
+    <el-container style="height:100%;margin:0;padding:0;">
+      <el-aside style="background-color: rgb(238, 241, 246);">
         <SysMenu></SysMenu>
       </el-aside>
-      <!-- </el-container> -->
       <el-container>
-        <el-header type="brand">海啸系统</el-header>
+        <el-header type="brand" style="background-color: rgb(238, 241, 246);">测试Header</el-header>
         <el-main>
           <router-view></router-view>
         </el-main>
@@ -28,4 +26,15 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+#app,
+html,
+body,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
 </style>
